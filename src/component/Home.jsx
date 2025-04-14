5be4eff0aee8c796c321f54a0d4c13fce9b8c4d6
MaurizioCr <crispinomaurizio034@gmail.com>
2025-04-14 17:10:58 +0200

    Home

---
+++ src/component/Home.jsx
@@ -0,0 +1,13 @@
+import CarouselComponent from './CarouselComponent';
+import CategorySection from './CategorySection';
+import WhatsAppIcon from './WhatsappIcon';
+
+export default function Home() {
+  return (
+    <>
+      <CarouselComponent />
+      <CategorySection />
+      <WhatsAppIcon />
+    </>
+  );
+}
