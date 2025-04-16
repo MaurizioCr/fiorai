7149b89002d3796df129b9225a4f1e6e19e4ae4e
MaurizioCr <crispinomaurizio034@gmail.com>
2025-04-16 19:40:55 +0200

    StickyNav

---
+++ src/component/StickyNavbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Navbar, Nav, Container, Image, Row, Col } from 'react-bootstrap';
+import logo from "../Assets/fantasie.jpeg"
+
+function StickyNavbar() {
+  return (
+    <>
+    <Navbar style={{background: "#EEAECA"}} expand="lg" sticky="top">
+      <Container>
+        {/* Logo del negozio */}
+        <Navbar.Brand href="/">
+          <Image
+            src={logo} 
+            alt="Logo del Negozio"
+            style={{width: "5em"}}
+            className="d-inline-block align-top rounded-circle"
+          />
+        </Navbar.Brand>
+
+        {/* Pulsante per il toggle su schermi piccoli */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        {/* Sezioni della navbar */}
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto"> {/* ms-auto spinge gli elementi a destra */}
+            <Nav.Link href="#in-primo-piano">In primo Piano</Nav.Link>
+            <Nav.Link href="#chi-siamo">Chi siamo</Nav.Link>
+            <Nav.Link href="#dove-trovarci">Dove trovarci</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <div className="bg-info text-white py-2 text-center">
+        <Container>
+          <Row>
+            <Col>
+              📢 Annuncio importante! Spedizione gratuita per ordini superiori a 50€!
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      </>
+  );
+}
+
+export default StickyNavbar;
