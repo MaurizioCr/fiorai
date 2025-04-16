fe898f022b40212779149b7648f7815fb1119bad
MaurizioCr <crispinomaurizio034@gmail.com>
2025-04-16 19:55:38 +0200

    fix bg

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div
     style={{
-      background: '#EEAECA',
+
       background: 'radial-gradient(circle, rgba(238, 174, 202, 1) 0%, rgba(148, 187, 233, 1) 100%)',
       minHeight: '100vh', // Opzionale: per far coprire l'intera viewport
     }}
