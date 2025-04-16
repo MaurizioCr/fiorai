d3e6906495580de733fa96089039469b14416029
MaurizioCr <crispinomaurizio034@gmail.com>
2025-04-16 19:42:06 +0200

    fix

--- src/component/CategorySection.jsx
+++ src/component/CategorySection.jsx
@@ -4,62 +4,51 @@ import { useNavigate } from 'react-router-dom';
 export default function CategorySection() {
   const navigate = useNavigate();
 
+  // Stile condiviso per immagini
+  const imageStyle = {
+    height: '10em',
+    objectFit: 'cover',
+  };
+
   return (
-    <Container className="mt-5">
+    <Container className="py-5">
+    <h2 className='text-center mb-3'>Le nostre collezioni</h2>
       <Row className="g-4">
         <Col md={3}>
-          <Card
-            style={{ cursor: 'pointer' }}
-            onClick={() => navigate('/categoria/pasquale')}
-          >
-            <Card.Img
-              variant="top"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc5UUAxIJ4dnI2Uf1g8-X5fQpj-ocxYiY1mg&s"
-            />
-            <Card.Body>
-              <Card.Title>Collezione Pasquale</Card.Title>
+          <Card style={{ cursor: 'pointer', height: '100%' }} onClick={() => navigate('/categoria/pasquale')} className="h-100">
+            <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc5UUAxIJ4dnI2Uf1g8-X5fQpj-ocxYiY1mg&s" style={imageStyle} />
+            <Card.Body className="text-center">
+              <Card.Title className="mb-0">Collezione Pasquale</Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col md={3}>
+          <Card style={{ cursor: 'pointer', height: '100%' }} onClick={() => navigate('/categoria/primaverile')} className="h-100">
+            <Card.Img variant="top" src="https://www.fioreriadassie.it/wp-content/uploads/2020/04/mazzo1.jpg" style={imageStyle} />
+            <Card.Body className="text-center">
+              <Card.Title className="mb-0">Collezione Primaverile</Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col md={3}>
+          <Card style={{ cursor: 'pointer', height: '100%' }} onClick={() => navigate('/categoria/piante-grasse')} className="h-100">
+            <Card.Img variant="top" src="https://pazienti-platform.s3.eu-central-1.amazonaws.com/zsdev2u9uibx00rww1jbw5abupxh" style={imageStyle} />
+            <Card.Body className="text-center">
+              <Card.Title className="mb-0">Piante Grasse</Card.Title>
             </Card.Body>
           </Card>
         </Col>
 
         <Col md={3}>
           <Card
-            style={{ cursor: 'pointer' }}
-            onClick={() => navigate('/categoria/primaverile')}
-          >
-            <Card.Img
-              variant="top"
-              src="https://www.fioreriadassie.it/wp-content/uploads/2020/04/mazzo1.jpg"
-            />
-            <Card.Body>
-              <Card.Title>Collezione Primaverile</Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-
-        <Col md={3}>
-          <Card
-            style={{ cursor: 'pointer' }}
-            onClick={() => navigate('/categoria/piante-grasse')}
-          >
-            <Card.Img
-              variant="top"
-              src="https://pazienti-platform.s3.eu-central-1.amazonaws.com/zsdev2u9uibx00rww1jbw5abupxh"
-            />
-            <Card.Body>
-              <Card.Title>Piante Grasse</Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-
-        <Col md={3}>
-          <Card
-            style={{ cursor: 'pointer' }}
+            style={{ cursor: 'pointer', height: '100%' }} className="h-100"
             onClick={() => navigate('/categoria/fiori')}
           >
             <Card.Img
               variant="top"
-              src="https://blog.interflora.it/wp-content/uploads/2022/04/primule.jpg"
+              src="https://blog.interflora.it/wp-content/uploads/2022/04/primule.jpg" style={imageStyle} 
             />
             <Card.Body>
               <Card.Title>Fiori</Card.Title>
