0f7f9f18392d1990bbe3142db786711a52a37e91
MaurizioCr <crispinomaurizio034@gmail.com>
2025-04-17 15:21:29 +0200

    z

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -5,7 +5,7 @@ import WhatsAppIcon from './WhatsappIcon';
 export default function Home() {
   return (
 <>
-<h1 className='text-center pt-5'>Fantasie Floreali</h1>
+<h1 className='text-center pt-5'>Fantasie Floreali.</h1>
       <CarouselComponent />
       <CategorySection />
       <WhatsAppIcon />
