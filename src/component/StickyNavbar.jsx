a641ccab4d86776e00a8634de04a0659db41cb36
MaurizioCr <crispinomaurizio034@gmail.com>
2025-04-17 15:46:22 +0200

    try fix 2

--- src/component/StickyNavbar.jsx
+++ src/component/StickyNavbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Navbar, Nav, Container, Image, Row, Col } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import logo from "../Assets/fantasie.jpeg"
 
 function StickyNavbar() {
+    const navigate = useNavigate();
   return (
     <>
     <Navbar style={{background: "#EEAECA"}} expand="lg" sticky="top">
@@ -11,9 +13,10 @@ function StickyNavbar() {
         <Navbar.Brand href="/">
           <Image
             src={logo} 
-            alt="Logo del Negozio"
+            alt="Logo Fantasie Floreali"
             style={{width: "5em"}}
             className="d-inline-block align-top rounded-circle"
+            
           />
         </Navbar.Brand>
 
@@ -25,7 +28,7 @@ function StickyNavbar() {
           <Nav className="ms-auto"> {/* ms-auto spinge gli elementi a destra */}
             <Nav.Link href="#in-primo-piano">In primo Piano</Nav.Link>
             <Nav.Link href="#chi-siamo">Chi siamo</Nav.Link>
-            <Nav.Link href="/dovetrovarci" >Dove trovarci</Nav.Link>
+            <Nav.Link onClick={() => navigate('/dovetrovarci')} >Dove trovarci</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
