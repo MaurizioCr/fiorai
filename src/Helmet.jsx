3928cedef051b015c45f35aaa4c590d976a10146
MaurizioCr <crispinomaurizio034@gmail.com>
2025-04-22 10:28:56 +0200

    Helmet

---
+++ src/Helmet.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from 'react-helmet';
+
+export default function Home() {
+  return (
+    <>
+      <Helmet>
+        <title>Fantasie Floreali | Frattaminore</title>
+        <meta name="description" content="Scopri le nostre nuove collezioni floreali di stagione." />
+      </Helmet>
+
+      {/* Contenuto */}
+    </>
+  );
+}
