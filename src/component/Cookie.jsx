503f07ecb80dd4e1047b44498994334d7eb4b0d9
MaurizioCr <crispinomaurizio034@gmail.com>
2025-04-29 16:08:07 +0200

    cookie page

---
+++ src/component/Cookie.jsx
@@ -0,0 +1,22 @@
+export default function Cookie (){
+    return(
+        <>
+            Cookie Policy
+Questo sito utilizza cookie tecnici e, previo consenso, cookie di terze parti.
+
+Cookie utilizzati:
+Google Analytics: monitoraggio delle visite in forma anonima
+
+WhatsApp Widget: per interagire via chat
+
+Google Fonts: per visualizzare correttamente i caratteri
+
+Come gestire i cookie
+È possibile disabilitare i cookie tramite le impostazioni del browser. Per saperne di più visita: www.aboutcookies.org
+
+Per maggiori informazioni scrivere a: info@fantasiefloreali.it
+
+
+        </>
+    )
+}
