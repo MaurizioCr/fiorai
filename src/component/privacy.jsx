8c5e1c8443f4c68c330275176ff27af1669f8a0b
MaurizioCr <crispinomaurizio034@gmail.com>
2025-04-29 16:06:12 +0200

    Pagina privacy

---
+++ src/component/privacy.jsx
@@ -0,0 +1,38 @@
+export default function Privacy() {
+    return (
+        <>
+Privacy Policy
+Titolare del Trattamento dei Dati:
+Fantasie Floreali di Lina Antignano
+Via Giovanni XXIII, 97, 80020 Frattaminore (NA)
+P. IVA: 01563890050
+Email: info@fantasiefloreali.it
+
+Tipologie di dati raccolti
+Fra i dati personali raccolti da questo sito ci sono: cookie, dati di utilizzo, email.
+
+Modalità e luogo del trattamento
+Il trattamento viene effettuato mediante strumenti informatici. I dati sono trattati presso la sede del titolare e in altri luoghi in cui si trovano le parti coinvolte.
+
+Finalità del trattamento
+I dati dell’utente sono raccolti per:
+
+Statistica (Google Analytics)
+
+Interazione tramite WhatsApp (click-to-chat)
+
+Visualizzazione di font esterni (Google Fonts)
+
+Diritti dell’utente
+L’utente ha diritto a:
+
+Accesso ai propri dati
+
+Rettifica o cancellazione
+
+Opporsi al trattamento
+
+Per esercitare i diritti, scrivere a: info@fantasiefloreali.it
+</>
+)
+}
